d7ca6def2cc4b12ab0c7fe1ae8807e8850bcba9c
Mark McLoughlin <markmc@redhat.com>
2016-10-30 20:29:29 +0000

    Add pelicanconf.py

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Mark McLoughlin'
+SITENAME = u"Crusty Blaa - Mark McLoughlin's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Dublin'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('twitter', 'http://twitter.com/markmc_'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
